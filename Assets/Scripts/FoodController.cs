258d57f08e15e072538ac55204f0a911b8b9f162
wasdty47 <mehemmed.memmedov1905@gmail.com>
2023-03-22 16:18:21 +0400

    Food and Snake changes

--- Assets/Scripts/FoodController.cs
+++ Assets/Scripts/FoodController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Food : MonoBehaviour
+public class FoodController : MonoBehaviour
 {
 
     public UnityEvent OnSnakeEat;
--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -2,22 +2,21 @@ using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-public class Snake : MonoBehaviour
+public class SnakeController : MonoBehaviour
 {
 
     [SerializeField] private Transform segmentPrefab;
-
     private float timeForStart = .3f;
     [SerializeField] private float timeForMove = .3f;
 
-    private Rigidbody2D rb; 
-    private Vector2 snakeDir = Vector2.left; // move left by deafult
+    private Rigidbody2D _rb; 
+    private Vector3 snakeDir = Vector2.left; // move left by deafult
 
     private List<Transform> segments = new List<Transform>();
 
     private void Start(){
         
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
         
         segments.Add(this.transform);
     
@@ -65,9 +64,13 @@ public class Snake : MonoBehaviour
 
         if(col.name == "food"){
 
-            Vector3 spawnPoint = new Vector3(segments[segments.Count-1].position.x, 
-                                             segments[segments.Count-1].position.y, 
-                                             transform.position.z) - (Vector3)snakeDir;
+            Vector3 spawnPoint;
+
+            if(segments.Count >= 2){
+                spawnPoint = segments[segments.Count-1].position - (segments[segments.Count-2].position - segments[segments.Count-1].position);
+            }else{
+                spawnPoint = transform.position-snakeDir;
+            }
 
             Transform segment = Instantiate(segmentPrefab, 
                                             spawnPoint, 
@@ -78,9 +81,7 @@ public class Snake : MonoBehaviour
         }else{
 
             SceneManager.LoadScene(0);
-
         }
     }
-
 
 }
